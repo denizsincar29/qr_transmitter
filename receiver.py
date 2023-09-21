3bb2dfadd6eec105f0d0859bd592beadaff5c5cc
Deniz Sincar <dsincar29@gmail.com>
2023-09-21 17:33:07 +0300

    colon

--- receiver.py
+++ receiver.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)  # just for debugging. after success we'll make webcam
 detector = cv2.QRCodeDetector()
 c=0
 a=''
-while c!=27
+while c!=27:
     _, img = cap.read()
     data, bbox, _ = detector.detectAndDecode(img)
     if data:
